f79edd6aa48df5c47a82606049ffb0ec6b2b8b89
nhan <48237932+levannhan228@users.noreply.github.com>
2020-03-10 13:43:35 +0700

    add babel

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require("express");
-var app=express();
-var hostname="localhost";
-var port=3000;
-app.get("/helloworld",(req,res)=>{
-    res.send("<h1>xin chao</h1>");
+import express from 'express';
+let app = express();
+let hostname = "localhost";
+let port = 3000;
+app.get("/", (req, res) => {
+  res.send("<h1>Hello word</h1>");
 });
 
-app.listen(port,hostname,()=>{
-    console.log(`hello nhan, i'm running at ${hostname}:${port}`)
+app.listen(port, hostname, () => {
+  console.log(`hello nhan, i'm running at ${hostname}:${port}`)
 });
