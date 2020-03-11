8e5a2f9f2b41af3c42166e30e05e35eb3c0ab6b2
nhan <48237932+levannhan228@users.noreply.github.com>
2020-03-11 13:58:31 +0700

    connect mongodb

--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
 import express from 'express';
+require('dotenv').config()
+import connectDB from './config/connectDB';
+import ContactModel from './models/contact.model';
 let app = express();
-let hostname = "localhost";
-let port = 3000;
-app.get("/", (req, res) => {
-  res.send("<h1>Hello word</h1>");
+
+//Connect Database
+connectDB();
+
+app.get("/test-database", async (req, res) => {
+  try {
+    let item = {
+      userId: "2323122132",
+      contacId: "2312321",
+    };
+    let contact = await ContactModel.createNew(item)
+    res.send(contact);
+  } catch (err) {
+    console.log(err)
+  }
 });
 
-app.listen(port, hostname, () => {
-  console.log(`hello nhan, i'm running at ${hostname}:${port}`)
+app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
+  console.log(`hello nhan, i'm running at ${process.env.APP_HOST}:${process.env.APP_PORT}`)
 });
