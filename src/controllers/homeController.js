626b57278ef5354042bce8bd239835f9ab445ee3
nhan <48237932+levannhan228@users.noreply.github.com>
2020-04-05 09:29:54 +0700

    done-realtime-notif

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,16 +1,31 @@
-import { notification } from './../services/index'
+import { notification, contact } from './../services/index'
 let getHome = async (req, res) => {
   //10 thông báo hiển thị
   let notifications = await notification.getNotifications(req.user._id)
   
   let countNotifUnread = await notification.countNotifUnread(req.user._id);
-
+  // list bạn bè
+  let contacts = await contact.getContacts(req.user._id);
+  // list gửi yêu cầu kết bạn
+  let contactsSent = await contact.getContactsSent(req.user._id);
+  // list xác nhận yêu cầu kêt bạn
+  let contactsReceived = await contact.getContactsReceived(req.user._id);
+  //
+  let countAllContacts = await contact.countAllContacts(req.user._id);
+  let countAllContactsSent = await contact.countAllContactsSent(req.user._id);
+  let countAllContactsReceived = await contact.countAllContactsReceived(req.user._id);
   return res.render("main/home/home", {
     errors: req.flash("errors"),
     success: req.flash("success"),
     user: req.user,
     notifications: notifications,
-    countNotifUnread : countNotifUnread
+    countNotifUnread : countNotifUnread,
+    contacts : contacts,
+    contactsSent : contactsSent,
+    contactsReceived : contactsReceived,
+    countAllContacts : countAllContacts,
+    countAllContactsSent : countAllContactsSent,
+    countAllContactsReceived : countAllContactsReceived,
   });
 };
 
